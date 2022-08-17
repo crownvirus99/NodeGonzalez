e5df0c711f4632bdc6846ab6eba4eb0a176f3ec7
crownvirus99 <martin.roca.99@gmail.com>
2022-08-17 13:52:30 -0400

    1 file modified

--- index.js
+++ index.js
@@ -2,7 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
-ReactDOM.render(
-  <h1>Hello, React!</h1>,
-  document.getElementById('root')
-);
+ReactDOM.render(<h1>pepo</h1>, document.getElementById('root'));
